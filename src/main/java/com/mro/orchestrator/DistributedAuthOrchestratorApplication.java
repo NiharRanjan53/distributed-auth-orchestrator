9b834fd9b3027b5e5c7f6dd823ab3ece1a4058b9
Nihar Ranjan <niharranjanrout53@gmail.com>
2026-05-02 17:00:57 +0530

    bootstrap distributed-auth-orchestrator with Java 21

---
+++ src/main/java/com/mro/orchestrator/DistributedAuthOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.mro.orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedAuthOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedAuthOrchestratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mro/orchestrator/DistributedAuthOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mro.orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedAuthOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
